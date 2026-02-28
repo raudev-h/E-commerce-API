4782c7170344c78dbb1a1c2474c8d7e59e43747c
raul <raulalbertohechavarria@gmail.com>
2026-02-28 13:03:46 -0500

    feat(category): add category model

---
+++ models/category.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from core.database import Base
+from uuid import uuid4, UUID
+
+class Category(Base):
+
+    __tablename__ = "category"
+
+    id:Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+
+    name:Mapped[str] = mapped_column(String(120))
+
+    description:Mapped[str | None] = mapped_column(String(300), nullable=True)
