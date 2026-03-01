2f12966dc6d86ab50013dc7a6d430c576fa9cfa6
raul <raulalbertohechavarria@gmail.com>
2026-02-28 19:54:19 -0500

    feat(category): add is_active field to model and response schema

--- models/category.py
+++ models/category.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from core.database import Base
 from uuid import uuid4, UUID
@@ -12,3 +12,5 @@ class Category(Base):
     name:Mapped[str] = mapped_column(String(120), unique=True)
 
     description:Mapped[str | None] = mapped_column(String(300), nullable=True)
+
+    is_active:Mapped[bool] = mapped_column(Boolean, default=True)
