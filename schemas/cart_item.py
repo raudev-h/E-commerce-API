2f8569aa497338cf196f9ff99b5b0ade5d0491ce
raul <raulalbertohechavarria@gmail.com>
2026-03-08 16:02:47 -0400

    feat(cart): add cart-item schema

---
+++ schemas/cart_item.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+from uuid import UUID
+from datetime import datetime
+
+class CartItemAdd(BaseModel):
+    user_id:UUID
+    product_id:UUID
+    quantity:int = Field(gt=0)
+
+class CartItemUpdate(BaseModel):
+    quantity:int = Field(gt=0)
+    
+class CartItemResponse(BaseModel):
+    id:UUID
+    cart_id:UUID
+    product_id:UUID
+    quantity:int
+    updated_at:datetime
+
+    model_config={"from_attributes":True}
