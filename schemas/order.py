d4864b804085a38c6694e470cbb3dc8c8d9b87e9
raul <raulalbertohechavarria@gmail.com>
2026-03-12 15:11:03 -0400

    feat(order): add order schema

---
+++ schemas/order.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from models.order import Status
+from uuid import UUID
+from datetime import datetime
+
+
+class OrderCreate(BaseModel):
+    user_id:UUID
+
+class OrderResponse(BaseModel):
+    id:UUID
+    user_id:UUID
+    status:Status
+    total:float
+    created_at:datetime
+    updated_at:datetime
+
+    model_config = {"from_attributes":True}
