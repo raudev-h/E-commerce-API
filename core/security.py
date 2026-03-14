95f3e4131b66f3efa72cb7feceeb7b720c025c00
raul <raulalbertohechavarria@gmail.com>
2026-03-14 13:38:39 -0400

    feat(security): add JWT token generation

--- core/security.py
+++ core/security.py
@@ -1,9 +1,27 @@
+import jwt
+from core.config import settings
 from pwdlib import PasswordHash
+from datetime import timedelta, timezone, datetime
 
 password_hash = PasswordHash.recommended()
+
+DUMMY_HASH = password_hash.hash("dummy") 
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def verify_password(plain_password, hashed_password):
     return password_hash.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
     return password_hash.hash(password)
+
+def create_access_token(data:dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=ALGORITHM)
+    return encoded_jwt
