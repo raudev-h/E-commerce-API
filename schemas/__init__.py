e4c891761ae876fd8fac571a336ab66ade284e7c
raul <raulalbertohechavarria@gmail.com>
2026-03-14 12:59:17 -0400

    feat(token): add token schema

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -4,3 +4,4 @@ from .product import ProductCreate, ProductUpdate, ProductResponse
 from .cart_item import CartItemAdd, CartItemResponse
 from .order import OrderCreate, OrderResponse
 from .order_item import OrderItemResponse
+from .token import Token
