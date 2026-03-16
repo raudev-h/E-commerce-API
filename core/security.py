80e499906da8f5b089f51fa7b0128f6ea0076e41
raul <raulalbertohechavarria@gmail.com>
2026-03-16 13:48:41 -0400

    feat(security): implement get_current_user with JWT verification

--- core/security.py
+++ core/security.py
@@ -1,9 +1,19 @@
 import jwt
+from jwt.exceptions import InvalidTokenError
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
 from core.config import settings
+from core.database import get_db
+from models import User
 from pwdlib import PasswordHash
+from typing import Annotated
 from datetime import timedelta, timezone, datetime
 
 password_hash = PasswordHash.recommended()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 DUMMY_HASH = password_hash.hash("dummy") 
 ALGORITHM = "HS256"
@@ -25,3 +35,24 @@ def create_access_token(data:dict, expires_delta: timedelta | None = None):
     to_encode.update({"exp":expire})
     encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=ALGORITHM)
     return encoded_jwt
+
+async def get_current_user(token:Annotated[str,Depends(oauth2_scheme)], db:AsyncSession = Depends(get_db)) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+        )
+    try:
+        payload = jwt.decode(token, settings.secret_key,algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except InvalidTokenError:
+        raise credentials_exception
+    
+    result = await db.execute(select(User).where(User.id == user_id))
+    user = result.scalar_one_or_none()
+    if not user:
+        raise credentials_exception
+    
+    return user
