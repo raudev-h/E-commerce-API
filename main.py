587569eacc70f848e2eb1f7132e965d4128d77bd
raul <raulalbertohechavarria@gmail.com>
2026-03-28 19:57:33 -0400

    feat(main): add global exception handlers

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from routers import user, category, product, cart_item, order, auth
+from core import NotFoundException, BadRequestException, ConflictException
 
 app = FastAPI()
+
+@app.exception_handler(NotFoundException)
+async def not_found_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail":str(exc)}
+    )
+
+@app.exception_handler(BadRequestException)
+async def bad_request_handler(request, exc):
+    return JSONResponse(
+        content={"detail":str(exc)},
+        status_code=exc.status_code
+    )
+
+@app.exception_handler(ConflictException)
+async def conflict_handler(request, exc):
+    return JSONResponse(
+        content={"detail":str(exc)},
+        status_code=exc.status_code
+    )
 
 app.include_router(user.router)
 app.include_router(category.router)
