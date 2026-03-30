a1e6b6e005658a570deb01cc3fa06cded2bf713a
raul <raulalbertohechavarria@gmail.com>
2026-03-30 16:41:43 -0400

    refactor(cart-item): remove try/except blocks from router

--- routers/cart_item.py
+++ routers/cart_item.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.database import get_db
 from models import User
@@ -12,21 +12,14 @@ router = APIRouter(prefix="/cart-item", tags=["cart_items"])
 
 @router.get("/", response_model=list[CartItemResponse])
 async def get_items(current_user:Annotated[User, Depends(security.get_current_user)],db:AsyncSession = Depends(get_db)):
-    try:
-        return await cart_item_service.get_cart_items(current_user.id,db)
-    except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+    return await cart_item_service.get_cart_items(current_user.id,db)
+
 
 @router.post("/", response_model=CartItemResponse, status_code= status.HTTP_201_CREATED)
 async def add_item(data:CartItemAdd, current_user: Annotated[User, Depends(security.get_current_user)] ,db:AsyncSession = Depends(get_db)):
-    try:
-        return await cart_item_service.add_item(current_user.id,data, db)
-    except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    return await cart_item_service.add_item(current_user.id,data, db)
+
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_item(id:UUID, current_user: Annotated[User, Depends(security.get_current_user)], db:AsyncSession = Depends(get_db)):
-    try:
-        await cart_item_service.delete_item(current_user.id, id, db)
-    except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+    await cart_item_service.delete_item(current_user.id, id, db)
