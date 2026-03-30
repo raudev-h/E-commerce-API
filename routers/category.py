b88286ef2b29082b76ec9a9d322411a6c8757b88
raul <raulalbertohechavarria@gmail.com>
2026-03-30 16:42:56 -0400

    refactor(category): remove try/except blocks from router

--- routers/category.py
+++ routers/category.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.database import get_db
 from core import security
@@ -18,30 +18,19 @@ async def get_categories(db:AsyncSession = Depends(get_db)):
 
 @router.get("/{id}", response_model=CategoryResponse)
 async def get_category(id:UUID, db:AsyncSession = Depends(get_db)):
-    try:
-        return await category_service.get_category_by_id(db, id)
-    except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+    return await category_service.get_category_by_id(db, id)
 
 
 @router.post("/", response_model=CategoryResponse, status_code=status.HTTP_201_CREATED)
 async def create_category(data:CategoryCreate, current_admin: Annotated[User, Depends(security.get_current_admin)], db:AsyncSession = Depends(get_db)):
-    try:
-        return await category_service.create_category(db, data)
-    except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e))
+    return await category_service.create_category(db, data)
 
 
 @router.patch("/{id}", response_model=CategoryResponse)
 async def update_user(id:UUID, data:CategoryUpdate, current_admin: Annotated[User, Depends(security.get_current_admin)], db:AsyncSession = Depends(get_db)):
-    try:
-        return await category_service.update_category(id, data, db)
-    except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e))    
+    return await category_service.update_category(id, data, db)
+
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_category(id:UUID, current_admin: Annotated[User, Depends(security.get_current_admin)] ,db:AsyncSession = Depends(get_db)):
-    try:
-        await category_service.delete_category(id, db)
-    except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+    await category_service.delete_category(id, db)
