5e5f17d80cd66164a6f2ad0fc27444c5523daba7
raul <raulalbertohechavarria@gmail.com>
2026-04-01 12:53:05 -0400

    chore(config): actualizar carga de .env con pydantic_settings

--- core/config.py
+++ core/config.py
@@ -1,24 +1,12 @@
 
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    database_url: str
-    secret_key:str
-    test_database_url:str
-    class Config:
-        env_file = ".env"
-
-settings = Settings() # type: ignore
-
-'''
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=str(Path(__file__).parent.parent / ".env"))
+    
     database_url: str
+    secret_key:str
+    test_database_url:str
 
-    class Config:
-        env_file = Path(__file__).parent.parent / ".env"
-
-settings = Settings()
-'''
+settings = Settings() # type: ignore
