d92173dd87713a80c6449b4ed5b0b12492869acd
raul <raulalbertohechavarria@gmail.com>
2026-04-05 15:23:01 -0400

    test(product): add product CRUD test

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,36 +1,33 @@
 import pytest
 from httpx import AsyncClient
+from uuid import uuid4
 
-'''
-crear un producto con un category_id inexistente
-actualizar un producto (cambiar description, nombre, stock) comprobar que solo cambiaron los campos concretos
-actualizar un producto isn ser admin
-soft delete -> is_active = False
-hacer get de un producto eliminado -> 404
-'''
 
-async def test_get_all_products(client:AsyncClient):
+async def test_get_all_products(client: AsyncClient):
     response = await client.get("/product/")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
-async def test_get_product_by_id(client:AsyncClient, created_product):
+
+async def test_get_product_by_id(client: AsyncClient, created_product):
     product_id = created_product["id"]
     response = await client.get(f"/product/{product_id}")
     assert response.status_code == 200
     assert response.json()["name"] == created_product["name"]
 
-async def test_get_product_wiht_wrong_id(client:AsyncClient, created_product):
+
+async def test_get_product_wiht_wrong_id(client: AsyncClient, created_product):
     response = await client.get("/product/00000000-0000-0000-0000-000000000000")
     assert response.status_code == 404
 
-async def test_create_product(auth_admin_client:AsyncClient, created_category):
+
+async def test_create_product(auth_admin_client: AsyncClient, created_category):
     product_data = {
-        "name":"iPhone 12 Pro Max",
-        "description":"2021 Apple smartphone",
-        "price":300,
-        "stock":10,
-        "category_id":created_category["id"]
+        "name": "iPhone 12 Pro Max",
+        "description": "2021 Apple smartphone",
+        "price": 300,
+        "stock": 10,
+        "category_id": created_category["id"],
     }
     response = await auth_admin_client.post("/product/", json=product_data)
     assert response.status_code == 201
@@ -40,24 +37,77 @@ async def test_create_product(auth_admin_client:AsyncClient, created_category):
     assert data["is_active"] == True
     assert "smartphone" in data["description"]
 
-async def test_create_duplicate_product(auth_admin_client:AsyncClient, created_product):
+
+async def test_create_duplicate_product(
+    auth_admin_client: AsyncClient, created_product
+):
     product_data = {
-        "name":"iPhone 12 Pro Max",
-        "description":"2021 Apple smartphone",
-        "price":300,
-        "stock":10,
-        "category_id":created_product["category_id"]
+        "name": "iPhone 12 Pro Max",
+        "description": "2021 Apple smartphone",
+        "price": 300,
+        "stock": 10,
+        "category_id": created_product["category_id"],
     }
     response = await auth_admin_client.post("/product/", json=product_data)
     assert response.status_code == 409
 
-async def test_create_product_without_been_admin(auth_client:AsyncClient, created_category):
+
+async def test_create_product_without_been_admin(
+    auth_client: AsyncClient, created_category
+):
     product_data = {
-        "name":"iPhone 12 Pro Max",
-        "description":"2021 Apple smartphone",
-        "price":300,
-        "stock":10,
-        "category_id":created_category["id"]
+        "name": "iPhone 12 Pro Max",
+        "description": "2021 Apple smartphone",
+        "price": 300,
+        "stock": 10,
+        "category_id": created_category["id"],
     }
     response = await auth_client.post("/product/", json=product_data)
     assert response.status_code == 403
+
+
+async def test_create_product_with_wrong_category_id(auth_admin_client: AsyncClient):
+    product_data = {
+        "name": "iPhone 12 Pro Max",
+        "description": "2021 Apple smartphone",
+        "price": 300,
+        "stock": 10,
+        "category_id": str(uuid4()),
+    }
+    response = await auth_admin_client.post("/product/", json=product_data)
+    assert response.status_code == 404
+
+    response = await auth_admin_client.get("/product/")
+    products = response.json()
+    assert not any(p["name"] == product_data["name"] for p in products)
+
+
+async def test_update_product(auth_admin_client: AsyncClient, created_product):
+    updated_data = {
+        "name": "iPhone 14 Pro Max",
+        "description": "iphone latest release",
+        "stock": 4,
+    }
+    response = await auth_admin_client.patch(
+        f"/product/{created_product["id"]}", json=updated_data
+    )
+    data = response.json()
+    assert response.status_code == 200
+    assert data["name"] == updated_data["name"]
+    assert data["description"] == updated_data["description"]
+    assert data["stock"] == updated_data["stock"]
+    assert data["price"] == created_product["price"]
+
+
+async def test_update_product_without_been_admin(auth_client: AsyncClient, created_product):
+    updated_data = {"name": "iPhone 14 Pro Max"}
+
+    response = await auth_client.patch(f"/product/{created_product["id"]}", json=updated_data)
+    assert response.status_code == 403
+
+async def test_soft_delete(auth_admin_client:AsyncClient, created_product):
+    response = await auth_admin_client.delete(f"/product/{created_product["id"]}")
+    assert response.status_code == 204
+
+    response = await auth_admin_client.get(f"/product/{created_product["id"]}")
+    assert response.status_code == 404
